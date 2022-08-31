3a9ee161465b2e6678f1a61a19a7358295130a96
ido-had <ido7410ido>
2022-08-31 14:25:02 +0300

    more fixture and base page basic methods

--- Pages/BasePage.py
+++ Pages/BasePage.py
@@ -10,16 +10,26 @@ class BasePg():
         self._driver = driver
         self.frameWork = frmwork
 
-    locators={"Backbtn":[(By.CLASS_NAME,"back"),"has text:BACK"]}
+    _locators={"Backbtn":[(By.CLASS_NAME,"back"),"has text:BACK"]}
 
     def getElement(self,locator):
         if self.frameWork==SELENIUM:
-            pass
+            return self._driver.find_element(locator[SELENIUM])
         else:
-            pass
+            try:
+                element= self._driver.locator(locator[PLAYWRIGHT])
+            except:
+                element= self._driver.query_selector(locator[PLAYWRIGHT])
+            finally:
+                return element
 
+    def paintElement(self,element):
+        if self.frameWork==PLAYWRIGHT:
+            element.evaluate("arguments[0].style.border='2px solid red'")
+        else:
+            self._driver.execute_script("arguments[0].style.border='2px solid red'",element)
 
 
 if __name__ == '__main__':
     locators = {"Backbtn": [(By.CLASS_NAME, "back"), "has text:BACK"]}
-    print(*locators.get("Backbtn")[1])
+    print(*locators.get("Backbtn")[0])
--- Tests/conftest.py
+++ Tests/conftest.py
@@ -4,12 +4,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import allure
 
-SELENIUM=0
-PLAYWRIGHT=1
+SELENIUM = 0
+PLAYWRIGHT = 1
 
 def pytest_addoption(parser):
     parser.addoption("--url", action="store", default="http://automationpractice.com/index.php")
-    parser.addoption("--drvrPath",action="store",default="C:\\Users\ohad\\Documents\\sela\\chromedriver_win32\\chromedriver.exe")
+    parser.addoption("--drvrPath",action="store",default="C:\\")
     parser.addoption("--frmwrk",action="store",default="s")
 
 @pytest.fixture(scope="session")
@@ -22,9 +22,73 @@ def getdriverPath(request):
 
 @pytest.fixture(scope="session")
 def getFrmwrk(request):
-    if request.config.getoption("--frmwrk") =="p":
+    if request.config.getoption("--frmwrk") == "p":
         return PLAYWRIGHT
     else:
         return SELENIUM
+@pytest.hookimpl( hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
+@pytest.fixture(scope="function")
+def browser(getFrmwrk):
+    if getFrmwrk==PLAYWRIGHT:
+        p = sync_playwright().start()
+        browser = p.chromium.launch(args=['--start-maximized'], headless=False)
+        return browser.new_page(no_viewport=True)
+    else:
+        pass
+      #  return webdriver.Chrome(executable_path=getChromePath)
+
+@pytest.fixture(scope="function")
+def web_browser(request,browser,getFrmwrk):
+   b=browser
+
+   # Return browser instance to test case:
+   yield b
+
+   # Do teardown (this code will be executed after each test):
+
+   if request.node.rep_call.failed:
+       # Make the screen-shot if test failed:
+       try:
+           b.screenshot(full_page=True, type="png")
+           allure.attach(b.screenshot(full_page=True, type="png"),
+                         name=request.function.__name__,
+                         attachment_type=allure.attachment_type.PNG)
+       except:
+           pass  # just ignore
+
+   # Close browser window:
+   b.close()
 
 
+# @pytest.fixture(scope="function")
+# def web_browser(request,getChromePath):
+#     # Open browser:
+#     b = webdriver.Chrome(executable_path=getChromePath)
+#     b.maximize_window()
+#     # Return browser instance to test case:
+#     yield b
+#
+#     # Do teardown (this code will be executed after each test):
+#
+#     if request.node.rep_call.failed:
+#         # Make the screen-shot if test failed:
+#         try:
+#             b.execute_script("document.body.bgColor = 'white';")
+#
+#             allure.attach(b.get_screenshot_as_png(),
+#                           name=request.function.__name__,
+#                           attachment_type=allure.attachment_type.PNG)
+#         except:
+#             pass # just ignore
+#
+#     # Close browser window:
+#     b.quit()
